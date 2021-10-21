4fbfbfaad5ea238504c1e8649c143567a9974720
baaluo <berycoin@gmail.com>
2021-10-22 02:06:26 +0500

    updated fees

--- constants/deposit_addresses.js
+++ constants/deposit_addresses.js
@@ -2,129 +2,129 @@ const DepositMTD = [
     {
         id: 1,
         deposit_system: "skrill",
-        deposit_min: 5,
+        deposit_min: 10,
         deposit_address: "support@voilk.com",
-        deposit_fee: "1.00 $"
+        deposit_fee: "0.00 $"
     },
     {
         id: 2,
         deposit_system: "neteller",
-        deposit_min: 5,
+        deposit_min: 10,
         deposit_address: "support@voilk.com",
-        deposit_fee: "1.00 $"
+        deposit_fee: "0.00 $"
     },
     {
         id: 3,
         deposit_system: "webmoney",
-        deposit_min: 5,
+        deposit_min: 10,
         deposit_address: "support@voilk.com",
-        deposit_fee: "1.00 $"
+        deposit_fee: "0.00 $"
     },
     {
         id: 4,
         deposit_system: "perfectmoney",
-        deposit_min: 5,
+        deposit_min: 10,
         deposit_address: "U9136632",
-        deposit_fee: "1.00 $"
+        deposit_fee: "0.00 $"
     },
     {
         id: 5,
         deposit_system: "tetherETH",
-        deposit_min: 50,
+        deposit_min: 10,
         deposit_address: "0x177Fd314A46400f596A17D08b22CEE9b6dCDE3aF",
-        deposit_fee: "1.00 USDT"
+        deposit_fee: "0.00 USDT"
     },
     {
         id: 6,
         deposit_system: "tetherTRON",
-        deposit_min: 50,
+        deposit_min: 10,
         deposit_address: "TQo7j3YHgG6FQUqPHnFKvjiAG44e7W2dZd",
-        deposit_fee: "1.00 USDT"
+        deposit_fee: "0.00 USDT"
     },
     {
         id: 7,
         deposit_system: "BitcoinCash",
-        deposit_min: 0.5,
+        deposit_min: 0.05,
         deposit_address: "qpuzl3z92kr0rp74ajxpeyt3alwjk5gwkcnpll0h8h",
-        deposit_fee: "0.02 BCH"
+        deposit_fee: "0.00 BCH"
     },
     {
         id: 8,
         deposit_system: "bitcoinSV",
-        deposit_min: 1,
+        deposit_min: 0.1,
         deposit_address: "1BJmnjudUrJnRzqZyAaQefXcD3Bnh441Qt",
-        deposit_fee: "0.1 BSV"
+        deposit_fee: "0.0 BSV"
     },
     {
         id: 9,
         deposit_system: "Litecoin",
-        deposit_min: 1,
+        deposit_min: 0.1,
         deposit_address: "ltc1qv4vxe6ps5ywk9m4ywk9rmxuv6e3zxjwhxgms46",
-        deposit_fee: "0.05 LTC"
+        deposit_fee: "0.00 LTC"
     },
     {
         id: 10,
         deposit_system: "Dash",
-        deposit_min: 1,
+        deposit_min: 0.1,
         deposit_address: "XeVUnEpxaY8v876734KuAj2JJrnniDQMXk",
-        deposit_fee: "0.1 DASH"
+        deposit_fee: "0.0 DASH"
     },
     {
         id: 11,
         deposit_system: "BitcoinGold",
-        deposit_min: 1,
+        deposit_min: 0.5,
         deposit_address: "btg1qqft609fh6a2z75p5h2m554znp94y8cuezpqd3j",
-        deposit_fee: "0.1 BTG"
+        deposit_fee: "0.0 BTG"
     },
     {
         id: 12,
         deposit_system: "Digibyte",
-        deposit_min: 2000,
+        deposit_min: 1000,
         deposit_address: "dgb1qzgaqfgpqu2vx3gkfcjlhphgtsdqlevzr2dyhjm",
-        deposit_fee: "100 DGB"
+        deposit_fee: "0 DGB"
     },
     {
         id: 13,
         deposit_system: "BNB",
-        deposit_min: 0.5,
+        deposit_min: 0.1,
         deposit_address: "bnb10c7xum67u23dgmamyzmx0g8mck9qrwz7vnqwk8",
-        deposit_fee: "0.05 BNB"
+        deposit_fee: "0.00 BNB"
     },
     
     {
         id: 14,
         deposit_system: "Dogecoin",
-        deposit_min: 500,
+        deposit_min: 100,
         deposit_address: "DCTJ6dTBJkcxsRb1UQk1uAixYLRC7q1jAr",
-        deposit_fee: "50 DOGE"
+        deposit_fee: "0 DOGE"
     },
     {
         id: 15,
         deposit_system: "ZCash",
-        deposit_min: 1,
+        deposit_min: 0.2,
         deposit_address: "t1QtztkweW18yD4hAPaRubQfZUhkHY1bedp",
-        deposit_fee: "0.1 ZEC"
+        deposit_fee: "0.0 ZEC"
     },
     {
         id: 16,
         deposit_system: "Tron",
-        deposit_min: 1000,
+        deposit_min: 100,
         deposit_address: "TQo7j3YHgG6FQUqPHnFKvjiAG44e7W2dZd",
-        deposit_fee: "50 TRX"
+        deposit_fee: "0 TRX"
     },
     {
         id: 17,
         deposit_system: "Ethereum",
-        deposit_min: 0.1,
+        deposit_min: 0.01,
         deposit_address: "0x177Fd314A46400f596A17D08b22CEE9b6dCDE3aF",
-        deposit_fee: "0.005 ETH"
+        deposit_fee: "0.00 ETH"
     },
     {
         id: 18,
         deposit_system: "Bitcoin",
-        deposit_min: 0.01,
+        deposit_min: 0.001,
         deposit_address: "bc1q4es6x3yf5p9vuj3umgnkrd9fjvd0hxl3m00y9s",
-        deposit_fee: "0.0005 BTC"
+        deposit_fee: "0.0000 BTC"
     }
 ]
 
